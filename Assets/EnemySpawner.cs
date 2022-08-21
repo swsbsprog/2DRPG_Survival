8a38c0847cf4fbfda638b9e149ee405c2538a4df
hktrollgames <hktrollgames@gmail.com>
2022-08-21 17:09:01 +0900

    Enemy스포너

---
+++ Assets/EnemySpawner.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemySpawner : MonoBehaviour
+{
+    public Enemy enemy;
+
+    public float interval = 1;
+    public float distance = 10;
+
+    IEnumerator Start()
+    {
+        while (true)
+        {
+            float angle = Random.Range(0, 360f);
+            Quaternion qrot = Quaternion.Euler(0, angle, 0);
+            Vector3 pos = qrot * new Vector3(0, 0, distance) + transform.position;
+            var newenemy = Instantiate(enemy, pos, Quaternion.identity);
+
+            yield return new WaitForSeconds(interval);
+        }
+    }
+}
